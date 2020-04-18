3427efd1f9be0585d336d0d0de3dffe58b57e1b8
Tim Taylor <tim@taylorhome.org>
2020-04-18 10:33:45 -0500

    Renaming projects

---
+++ person-service/src/main/java/com/tenkode/person/dto/PersonDto.java
@@ -0,0 +1,22 @@
+package com.tenkode.person.dto;
+
+public class PersonDto {
+	private String first;
+	private String last;
+
+	public String getFirst() {
+		return first;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	public void setFirst(String first) {
+		this.first = first;
+	}
+
+	public void setLast(String last) {
+		this.last = last;
+	}
+}
