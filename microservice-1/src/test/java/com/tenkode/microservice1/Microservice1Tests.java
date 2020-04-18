656e8402da6cced6c5f72ad419ef8e01db0bc366
Tim Taylor <tim@taylorhome.org>
2020-04-18 09:50:54 -0500

    Intial population of monorepo master branch and addition of files

---
+++ microservice-1/src/test/java/com/tenkode/microservice1/Microservice1Tests.java
@@ -0,0 +1,16 @@
+package com.tenkode.microservice1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Microservice1Tests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
